3f30eed214a7592598dcaa661e83e9ff46b9b015
Kevin Susanto <kevin.stnt@gmail.com>
2019-05-17 15:52:39 +0700

    Initial Commit

---
+++ Exercise Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercise Animation
+//
+//  Created by Kevin Susanto on 17/05/19.
+//  Copyright © 2019 Kevin Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
