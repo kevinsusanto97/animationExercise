cd6aed777375e35a5ad6582b5c8017ceba7cb8ca
Kevin Susanto <kevin.stnt@gmail.com>
2019-05-17 17:17:23 +0700

    Add Shape + Basic Move

--- Exercise Animation/ViewController.swift
+++ Exercise Animation/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var bolaBtn: UIButton!
+    @IBAction func bolaBtn(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.55, animations: {
+            self.bolaBtn.frame = CGRect(x: 220, y: 220, width: 100, height: 100)
+            self.bolaBtn.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.bolaBtn.layer.cornerRadius = 50
         // Do any additional setup after loading the view.
     }
 
